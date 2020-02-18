1709ac42868eaa9146cb2dc8eedb242ba56d065a
Basheer Alhader <basheer@beamwallet.me>
2020-02-18 11:44:49 +0200

    adding framework

---
+++ QReader.framework/Headers/MAFPayQRReader.h
@@ -0,0 +1,19 @@
+//
+//  QReader.h
+//  QReader
+//
+//  Created by Basheer AlHader on 1/21/20.
+//  Copyright © 2020 Basheer AlHader. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QReader.
+FOUNDATION_EXPORT double QReaderVersionNumber;
+
+//! Project version string for QReader.
+FOUNDATION_EXPORT const unsigned char QReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QReader/PublicHeader.h>
+
+
